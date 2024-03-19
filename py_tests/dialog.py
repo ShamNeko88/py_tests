ba55a19c580a9e842e94cf0a8d28930c93d7ef92
ShamNeko <mkdvlp01@gmail.com>
2024-03-19 23:51:38 +0900

    ダイアログテスト

--- py_tests/dialog.py
+++ py_tests/dialog.py
@@ -1,6 +1,17 @@
 """
+simpledialog
 ダイアログ表示テスト
 """
 from tkinter import simpledialog as sd
 
-sd.askstring("", "test")
+# 文字列受付
+a = sd.askstring("", "askstring")
+# 整数受付
+b = sd.askinteger("", "askinteger")
+# 浮動小数点受付
+c = sd.askfloat("", "askfloat")
+
+# 出力
+print(f"a:{a}")
+print(f"b:{b}")
+print(f"c:{c}")
